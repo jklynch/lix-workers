3c2d4484bb0157200a710a3d0419e9dba05504b6
Joshua Lynch <jlynch@bnl.gov>
2020-03-12 12:40:13 -0400

    zero-pad chunk number

--- export/lix_export_worker.py
+++ export/lix_export_worker.py
@@ -85,7 +85,7 @@ class Packer(DocumentRouter):
         i = next(self.chunk_counter)
         filename = (
             f"{md['uid'][:8]}_"
-            f"chunk{i}_"
+            f"chunk{i:02}_"
             f"{md.get('sample_name', 'sample_name_not_recorded')}"
         )
         print(f"Writing {filename} with shape {image_stack.shape}...")
